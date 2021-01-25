46e93cc3a46379289983146fef7d3ecad2a725db
Markusiths <71008779+Markusiths@users.noreply.github.com>
2021-01-25 18:16:42 +0100

    hela lab1. hoppas jag

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -7,20 +7,26 @@ namespace Lab1
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.White;
+            //ändrar textens färg till vit
 
-            Console.WriteLine("Skriv in en massa skit:");
+            Console.Write("Please Enter Your input Here: ");
 
             string input = Console.ReadLine();
             long totalSum = 0;
+            //startar på "0" för att få första input-siffran på startindex "0" dvs första möjliga startposition
             for (int firstIndex = 0; firstIndex < input.Length; firstIndex++)
+                //for-loop för att få hela maskineriet i rörelse. Fortsätter så länge "firstIndex" är mindre (i form av antal tecken) än det som användaren angett och ökar med 1 för varje loop.
             {
                 if (!char.IsNumber(input, firstIndex))
+                    //om "input" inte är en siffra på "firstIndex" position så går den vidare till nästa instruktion
                 {
                     continue;
                 }
                 for (int lastIndex = firstIndex + 1; lastIndex < input.Length; lastIndex++)
+                    //for-loop för "lastIndex". dvs loopen som letar sista siffran för att sedan skapa ett tal utifrpn två lika siffror
                 {
-                    if (!char.IsNumber(input[lastIndex]))
+                    if (!char.IsNumber(input[lastIndex])) //varför används "[]" i denna if-loopen. betyder det att "lastIndex" är en array?
+                        
                     { 
                         break;
                     }
@@ -57,6 +63,7 @@ namespace Lab1
             }
             Console.WriteLine();
             Console.WriteLine($"Totalen är: {totalSum}");
+            
         }
     }
 }
